72f56a45b2d13d9de028a3ddc02338ca1597fe01
MalikZone <ridwanmalik2010@gmail.com>
2020-03-05 14:17:41 +0700

    modul create part2

--- resources/js/components/Create.vue
+++ resources/js/components/Create.vue
@@ -25,3 +25,33 @@
        </div>
     </div>
 </template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data: function() {
+            return {
+                posts: {
+                    title: "",
+                    description: ""
+                },
+                errors: []
+            }
+        },
+
+        // Fetches posts when the component is created.
+        methods:{
+            submitPost(){
+                axios.get(`http://jsonplaceholder.typicode.com/posts`)
+                .then(response => {
+                    // JSON responses are automatically parsed.
+                    this.posts = response.data
+                })
+                .catch(e => {
+                    this.errors.push(e)
+                })
+            }
+        }
+    }
+</script>
